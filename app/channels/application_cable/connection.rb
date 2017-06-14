f4e63553a875eed43acda00fc42b669a13917fa3
Mauro Berlanda <mauro.berlanda@mecglobal.com>
2017-06-14 12:25:45 +0200

    Added User Verification

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,15 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    include SessionsHelper
+    identified_by :message_user
+
+    def connect
+      self.message_user = find_verified_user
+    end
+
+    def find_verified_user
+      logged_in? ? current_user : reject_unauthorized_connection
+    end
+
   end
 end
