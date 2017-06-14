9caf6ef23f81b8c5d67eef98501f75704b96d5c9
Mauro Berlanda <mauro.berlanda@mecglobal.com>
2017-06-14 16:30:47 +0200

    Allowed all actioncable requests

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # allow all websockets requests
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 end
